904d8993dce7b10375f064068a5652ac1f93b1d8
dengtao <dengtao@ykxn.net>
2021-08-06 11:45:26 +0800

    2

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -1,65 +1,74 @@
+const db = wx.cloud.database()
 Page({
+	/**
+	 * 页面的初始数据
+	 */
+	data: {},
+	getData() {
+		db.collection('demoList')
+			// 是不同数据下的细筛
+			.where({ name: '苹果' })
+			// 只能查询id
+			// .doc("5b5be87f610c939300d790357f5491b6")
+			.get()
+			.then(res => console.log(res))
+	},
+	addData() {
+		wx.showLoading({
+			title: '数据加载中，稍等！'
+		})
+		db.collection('demoList')
+			.add({
+				data: {
+					name: '桃子',
+					number: 77
+				}
+			})
+			.then(res => {
+				wx.hideLoading({
+					complete: res => {
+						console.log('数据加载完成')
+					}
+				})
+			})
+	},
+	/**
+	 * 生命周期函数--监听页面加载
+	 */
+	onLoad: function (options) {},
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
-    
-  },
+	/**
+	 * 生命周期函数--监听页面初次渲染完成
+	 */
+	onReady: function () {},
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-    
-  },
+	/**
+	 * 生命周期函数--监听页面显示
+	 */
+	onShow: function () {},
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-    
-  },
+	/**
+	 * 生命周期函数--监听页面隐藏
+	 */
+	onHide: function () {},
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-    
-  },
+	/**
+	 * 生命周期函数--监听页面卸载
+	 */
+	onUnload: function () {},
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-    
-  },
+	/**
+	 * 页面相关事件处理函数--监听用户下拉动作
+	 */
+	onPullDownRefresh: function () {},
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-    
-  },
+	/**
+	 * 页面上拉触底事件的处理函数
+	 */
+	onReachBottom: function () {},
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-    
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-    
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-    
-  }
+	/**
+	 * 用户点击右上角分享
+	 */
+	onShareAppMessage: function () {}
 })
